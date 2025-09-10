a08f55644dfa326bfaee82b32c8b30ae4bf1da49
qubicbit <sis.esse161@outlook.com>
2025-09-10 15:46:22 +0200

    changed again

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello!");
             // Testar PR-template.md
         }
     }
